ed38616a34584b96c14883a8e7fa127b490ef730
Hafsat Bakare <105776874+Hafsat-Bakare@users.noreply.github.com>
2026-04-28 17:48:00 -0400

    Update update-role.ps1

--- scripts/mover/update-role.ps1
+++ scripts/mover/update-role.ps1
@@ -1,1 +1,15 @@
+Remove-ADGroupMember `
+  -Identity "groupname" `
+  -Members "user1" `
+  -Confirm:$false
 
+Add-ADGroupMember `
+  -Identity "newgroupname" `
+  -Members "user1"
+
+Set-ADUser `
+  -Identity "user1" `
+  -Department "new department" `
+  -Title "new title"
+
+Move-ADObject -Identity "Distinguished name of user1" -TargetPath "new OU path for user1"
