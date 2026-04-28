b8d31879c9157832faa327e7dbee140a448be1c1
Hafsat Bakare <105776874+Hafsat-Bakare@users.noreply.github.com>
2026-04-28 17:45:42 -0400

    Update disable-user.ps1

--- scripts/leaver/disable-user.ps1
+++ scripts/leaver/disable-user.ps1
@@ -1,1 +1,9 @@
+Disable-ADAccount -Identity "User1"
 
+Get-ADUser "user1" -Properties MemberOf |
+  Select-Object -ExpandProperty MemberOf |
+  Remove-ADGroupMember -Members "user1" -Confirm:$false
+
+Move-ADObject `
+  -Identity (Get-ADUser "user1").DistinguishedName `
+  -TargetPath "OU=Disabled Users,OU=Haftech_Users,DC=hafsatsoc,DC=lab"
