295b16c7347747b1bdfdf3b582a3a324806b1dbe
Hafsat Bakare <105776874+Hafsat-Bakare@users.noreply.github.com>
2026-04-28 17:44:39 -0400

    Update create-user.ps1

--- scripts/joiner/create-user.ps1
+++ scripts/joiner/create-user.ps1
@@ -1,1 +1,11 @@
+New-ADUser `
+  -Name "User1" `
+  -SamAccountName "User1" `
+  -Path "OU=Operations,OU=Company Staff,OU=Haftech_Users,DC=hafsatsoc,DC=lab"`
+  -AccountPassword (ConvertTo-SecureString "password!" -AsPlainText -Force) `
+  -Enabled $true `
+  -Department "Operations" `
+  -Title "Operations Support" `
 
+
+Add-ADGroupMember -Identity "Groupname" -Members "User1"
